05520ef4e6f10dc84f021997446403e76ccca79a
Julio Acuña <urkonn@gmail.com>
2015-10-14 20:58:52 -0500

    Adds harvest endpoint

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
+import logging
 from flask import Flask
 from flask_restful import Api
 from api.v1.resources.ping import Ping
 from api.v1.resources.finddatasets import FindDatasets
 from api.v1.resources.createorganization import CreateOrganization
+from api.v1.resources.harvest import Harvest
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,6 +13,7 @@ api = Api(app)
 api.add_resource(Ping, '/v1/ping')
 api.add_resource(FindDatasets, '/v1/finddataset')
 api.add_resource(CreateOrganization, '/v1/createorg')
+api.add_resource(Harvest, '/v1/harvest')
 
 handler = logging.FileHandler(filename='app.log', mode='w')
 handler.setLevel(logging.INFO)
