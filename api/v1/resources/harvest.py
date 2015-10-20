212f494c8417b3621246ff8fa805785e8f62b6bb
Julio Acuña <urkonn@gmail.com>
2015-10-20 14:47:01 -0500

    create organization before sync

--- api/v1/resources/harvest.py
+++ api/v1/resources/harvest.py
@@ -1,4 +1,5 @@
 import os
+from urlparse import urlparse
 from flask import jsonify
 from flask_restful import Resource, reqparse
 import ckanapi
@@ -15,6 +16,12 @@ class Harvest(Resource):
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument('url', type=str, required=True)
         arg_url = parser.parse_args()
+        org_url = urlparse(arg_url['url'])
+        org_name = org_url.path.split("/")[1]
+        try:
+            remote.action.organization_create(name=org_name)
+        except ckanapi.ValidationError:
+            pass
         catalog = dcat_to_utf8_dict(arg_url['url'])
         for dcat_dataset in catalog.get('dataset', []):
             ckan_dataset = converters.dcat_to_ckan(dcat_dataset)
