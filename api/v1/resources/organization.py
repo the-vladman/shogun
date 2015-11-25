0df7f1fadd6ba98f2f2152b75276eb1e52d3e347
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-11-24 19:19:57 -0600

    Will not return None types
    + Adding tests

--- api/v1/resources/organization.py
+++ api/v1/resources/organization.py
@@ -1,7 +1,15 @@
 from flask_restful import Resource, reqparse
 
 
+def xstr(s):
+    return '' if s is None else str(s)
+
+
 class Organization(Resource):
+    def exists_in_remote(self, name):
+        return True
+
+
     # Updates metadata for an organization
     # See http://docs.ckan.org/en/latest/api/
     # index.html#ckan.logic.action.update.organization_update
@@ -11,10 +19,19 @@ class Organization(Resource):
         parser.add_argument('title')
         parser.add_argument('description')
         query = parser.parse_args()
-        return {'organization': {
-                'name'          : query['name'],
-                'title'         : query['title'],
-                'description'   : query['description']
-            }}
+
+        # Default to RESTful parameter
+        organization_name = name
+        if not (query['name'] is None):
+            organization_name = query['name']
+
+        if self.exists_in_remote(name=name):
+            return {'organization': {
+                    'name'          : organization_name,
+                    'title'         : xstr(query['title']),
+                    'description'   : xstr(query['description'])
+                }}
+        else:
+            return {"message": "Organization not found in remote host"}
 
 
--- api/v1/tests/tests.py
+++ api/v1/tests/tests.py
@@ -7,6 +7,32 @@ class PingTestCase(TestCase):
         app.config.from_object('config.TestConfig')
         return app
 
+
     def test_ping(self):
         response = self.client.get('/v1/ping')
         self.assertEqual(response.json, {'ping': 'pong'})
+
+
+    def test_update_returns_organization_with_new_values(self):
+        attributes = dict(
+            name='fubar',
+            title='Foo Inc.',
+            description='Beyond recognition'
+        )
+        expected = {u'organization': attributes}
+        response = self.client.put('/v1/organization/fubar', data=attributes)
+        self.assertEqual(response.json, expected)
+
+
+    def test_grabs_name_from_rest_parameter_as_default(self):
+        expected = {
+            u'organization': {
+                u'name'         : u'fubar',
+                u'title'        : u'',
+                u'description'  : u''
+            }
+        }
+        response = self.client.put('/v1/organization/fubar')
+        self.assertEqual(response.json, expected)
+
+
