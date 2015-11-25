6b3640574f9398053634bdfcf40ac5e3cb2bf80c
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-11-24 19:23:30 -0600

    Namespacing organization tests

--- api/v1/tests/tests.py
+++ api/v1/tests/tests.py
@@ -13,6 +13,12 @@ class PingTestCase(TestCase):
         self.assertEqual(response.json, {'ping': 'pong'})
 
 
+class OrganizationTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
+
+
     def test_update_returns_organization_with_new_values(self):
         attributes = dict(
             name='fubar',
