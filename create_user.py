5bd774f8c2994d742bab9d9d79041bf732565287
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2015-12-05 15:34:24 -0600

    return token when the user is created

--- create_user.py
+++ create_user.py
@@ -1,17 +1,14 @@
-import os
-from flask.ext.sqlalchemy import SQLAlchemy
 from api.app import app,db
 from users import User
 
-passwd=app.config['PASSWORD']
-username=app.config['USER']
-
 if __name__ == '__main__':
     app.config.from_object('config.TestConfig')
-    #if not os.path.exists('db.sqlite'):
+    passwd=app.config['PASSWORD']
+    username=app.config['USER']
     with app.app_context():
         db.create_all()
     user = User(username=username,config=app.config)
     user.hash_password(passwd)
     db.session.add(user)
     db.session.commit()
+    print user.generate_auth_token()
