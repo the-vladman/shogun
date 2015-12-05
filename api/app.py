54284a8ab74f6ee00e036f0dffb996863cc951ca
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2015-12-05 15:35:17 -0600

    fix token class, to query the token via http

--- api/app.py
+++ api/app.py
@@ -1,13 +1,12 @@
 import logging
 from flask import Flask, g
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource
 from api.v1.resources.ping import Ping
 from api.v1.resources.finddatasets import FindDatasets
 from api.v1.resources.createorganization import CreateOrganization
 from api.v1.resources.harvest import Harvest
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.httpauth import HTTPBasicAuth
-from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
@@ -24,11 +23,15 @@ class Token(Resource):
         super(Token, self).__init__()
 
     def get(self):
-        passwd=app.config['PASSWORD']
         username=app.config['USER']
-        user = User(username,app.config)
-        token = user.generate_auth_token()
-        return {'token': token.decode('ascii')}
+        s = Serializer(app.config['USER'])
+        try:
+            user = User.query.filter_by(username=username).first()
+        except SignatureExpired:
+            return None
+        except BadSignature:
+            return None
+        return s.dumps({'id': user.id})
 
 
 @auth.verify_password
@@ -40,14 +43,13 @@ def verify_password(username_or_token, password):
             return False
     g.user = user
     app.logger.debug(username_or_token)
-    app.logger.debug(password)
     return True
 
 api = Api(app=app)
 
-api.add_resource(Token, '/token')
 
 api = Api(app=app,decorators=[auth.login_required])
+api.add_resource(Token, '/token')
 api.add_resource(Ping, '/v1/ping')
 api.add_resource(FindDatasets, '/v1/finddataset')
 api.add_resource(CreateOrganization, '/v1/createorg')
