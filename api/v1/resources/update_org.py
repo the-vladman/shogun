fa5fc8c9a8156f12e02f3f966c5a528f6117131a
Omar Vladimir Galicia Palmeros <thewakosmanclub@gmail.com>
2018-04-03 23:29:46 -0500

    post to CKAN

--- api/v1/resources/update_org.py
+++ api/v1/resources/update_org.py
@@ -10,8 +10,15 @@ remote = ckanapi.RemoteCKAN(HOST, user_agent='ckanops/1.0', apikey=TOKEN)
 class UpdateOrg(Resource):
     def post(self):
         parser = reqparse.RequestParser(bundle_errors=True)
-        parser.add_argument('oldname', type=str, location='args')
-        parser.add_argument('newname', type=str, location='args')
+        parser.add_argument('oldname', type=str, required=True)
+        parser.add_argument('newname', type=str, required=True)
         query = parser.parse_args()
-        data = { 'name': "newname", 'id':query['oldname'] }
-        return data
+        data = { 'name': query['newname'], 'id':query['oldname'] }
+        try:
+            remote.call_action('organization_update', data)
+            return {'Organization Updated': query['newname']}
+        except ckanapi.ValidationError as e:
+            if str(e) == "{u'__type': u'Validation Error', u'name': [u'Group name already exists in database']}":
+                return {'Validation Error': 'Organization already exists'}
+            else:
+                return {'Error': 'Something went wrong'}
